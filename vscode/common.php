6b1f2aea6d7aab0bbc7c5c296cfe492fe8b76d6b
test <test@example.com>
2019-05-26 23:32:45 +0800

    go

--- vscode/common.php
+++ vscode/common.php
@@ -1,9 +1,21 @@
+文件家名称 .vscode
 打开新的文件会覆盖窗口中的,怎么改  "workbench.editor.enablePreview": false
+settings.json
+// 系统配置1
 {
 "files.autoSave": "afterDelay",
 "workbench.editor.enablePreview": false
 }
+// 系统配置2
+{
+    "files.autoSave": "afterDelay",
+    "workbench.editor.enablePreview": false,
+    "git.autofetch": true,
+    "editor.tabSize": 2,
+    "terminal.integrated.shell.windows": "C:\\Windows\\System32\\cmd.exe"
+}
 
+// 项目配置
 {
     "go.useLanguageServer": true,
     "go.languageServerExperimentalFeatures": {
@@ -25,3 +37,17 @@
         "-unsafeptr"
     ]
 }
+
+koroFileHeader // 自动注释 
+文件头部添加注释:
+
+在文件开头添加注释，记录文件信息
+支持用户高度自定义注释选项
+保存文件的时候，自动更新最后的编辑时间和编辑人
+快捷键：window：ctrl+alt+i,mac：ctrl+cmd+i
+
+在光标处添加函数注释:
+
+在光标处自动生成一个注释模板，下方有栗子
+支持用户高度自定义注释选项
+快捷键：window：ctrl+alt+t,mac：ctrl+cmd+t
